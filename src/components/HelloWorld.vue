7627e8f533fe41a308c75f2750496c3416cb511d
鲁正佼 <3038413299@qq.com>
2022-10-17 20:47:28 +0800

    对项目进行基本配置

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+
+<template>
+<h2 class="home">{{count}}</h2>
+<button @click="handleClick">添加</button>
+
+<el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+</template>
+
+
+<script setup lang="ts">
+import {ref,computed} from 'vue'
+// 引入
+import {useStore} from '../store'
+const num = ref(0)
+const store = useStore()
+const count =computed(()=>{
+   return store.getters.getCount
+})
+
+//添加事件
+const handleClick=()=>{
+  num.value+=1
+   store.commit("setCount",num.value)
+}
+// const count = ref(0)
+</script>
+
+<style lang="scss" scoped>
+.home{
+  color: blue;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+// 导入路由
+import router from './router/index'
+
+import {store, key} from './store/index'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+const app =createApp(App)
+app.use(router)
+app.use(store,key)
+app.use(ElementPlus)
+app.mount('#app')
